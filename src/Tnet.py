b7007ba4c68b0d72cbcccdfceb396682b0f52793
Lorenzo Dall'Amico <46966964+lorenzodallamico@users.noreply.github.com>
2025-03-31 12:30:24 +0200

    uptnet

--- src/Tnet.py
+++ src/Tnet.py
@@ -97,3 +97,172 @@ def ComputeIntereventStatistic(df_):
             intervals.append((ddf.t + ddf.τ - np.roll(ddf.t, 1))[1:].values)
    
     return np.concatenate(intervals)
+
+
+def ActivityDriven(n, x, η, m, T, verbose = True):
+    '''Generates an instance of the activty driven model
+    
+    Use: dfttau, el = ActivityDriven(n, x, η, m, T)
+    
+    Inputs:
+        * n (int): number of nodes
+        * x (array): activity potentials
+        * η (float): activity parameter
+        * m (int): number of edges per node
+        * T (int): number of snapshots
+
+    Optional inputs:
+        *verbose (bool): is True (default) is prints the progress
+
+    Output:
+        * dfttau (pandas DataFrame): temporal graph in the format i, j, t, τ
+        * el (dictionary): The keys are edge (i,j) with i < j and the entries are lists of lists in the format [t_i, t_f], where t_i denotes the beginning of an interaction and t_f the end. Note that t_f - t_i >= 1. 
+    '''
+
+    # initialize the dictionary containing the contact timelines of each edge. 
+    el = dict()
+
+    for t in range(T):
+        if verbose:
+            print(f'{int((t+1)*100/T)}%', end = '\r')
+
+        # choose which nodes are active at a particular time-step
+        active = np.where(np.random.binomial(1, η*x) == 1)[0]
+
+        for u in active:
+            # select the neighbors of the active nodes and add the contacts
+            # choose the neighbor among all nodes except u
+            neighbors = np.random.choice(list(set(np.arange(n)) - set([u])), m, replace = False)
+            for v in neighbors:
+                el = _add_contact(el, u, v, t)
+
+    # convert to the ijttau format
+    el_ = []
+
+    for e in el.keys():
+        i, j = e
+        for tt in el[e]:
+            el_.append([i, j, tt[0], tt[1]-tt[0]])
+
+    # construct the dataframe in the form ijttau
+    dfttau = pd.DataFrame(el_, columns = ['i', 'j', 't', 'τ'])
+
+    return dfttau, el
+
+
+def _add_contact(el, u, v, t):
+    '''This function adds a contact to the dictionary el'''
+
+    # write the edge (u,v) so that u < v
+    e = tuple([np.min([u, v]), np.max([u, v])])
+
+    
+    if e in el.keys():
+        # if the contact is ongoing, increase its duration
+        if el[e][-1][1] == t:
+            el[e][-1][1] = t+1
+        else:
+            # if the contact is not ongoing create a new one
+            el[e].append([t, t+1])
+    
+    else:
+        # if e is not in the dictionary, add it and set the contact duration to 1
+        el[e] = [[t, t+1]]
+
+    return el
+
+
+def getInterEvent(el):
+    '''This function computes the inter-event statistics from the dictionary el'''
+
+    ied = []
+
+    for e in el.keys():
+        
+        if len(el[e]) > 1:
+            for i in range(len(el[e])-1):
+                ied.append(el[e][i+1][0] - el[e][i][1])
+
+    return np.array(ied)
+
+
+def ActractivenessModel(n, L, a, rv, step, r, T, verbose = True):
+    '''This function generates an instance of the attractiveness model
+
+    Use: dft, el = ActractivenessModel(n, L, a, rv, step, r, T, verbose = True)
+    
+    Inputs:
+        * n (int): number of nodes
+        * L (float): box size
+        * a (array): attractivity parameters
+        * rv (array): activity vector
+        * step (float): length of a step in the movement
+        * r (float): radius that defines proximity
+        * T (int): number of time-steps
+
+    Optional inputs:
+        * verbose (bool): if True (default) the function prints the the progress
+
+    Outputs:
+        * dfttau (pandas DataFrame): temporal graph in the format i, j, t, τ
+        * el (dictionary): The keys are edge (i,j) with i < j and the entries are lists of lists in the format [t_i, t_f], where t_i denotes the beginning of an interaction and t_f the end. Note that t_f - t_i >= 1. 
+    '''
+
+    # initialize the positions of the agents
+    xpos = np.random.uniform(0, L, n)
+    ypos = np.random.uniform(0, L, n)
+
+    # initialize the dictionary containing the contact timelines of each edge. The keys are edge (i,j) with i < j and the entries are lists of lists in the format
+    # [t_i, t_f], where t_i denotes the beginning of an interaction and t_f the end. Note that t_f - t_i >= 1.
+    el = dict()
+
+    for t in range(T):
+        if verbose:
+            print(f'{int((t+1)/T*100)}%', end = '\r')
+
+        # probability of not moving
+        p_not_move = np.zeros(n)
+
+        # identify active nodes
+        active_nodes = np.where(np.random.binomial(1, rv) == 1)[0]
+
+
+        for i in active_nodes:
+            for j in active_nodes:
+                if i < j:
+                    # check the distance
+                    if (xpos[i] - xpos[j])**2 + (ypos[i] - ypos[j])**2 < r**2:
+                        
+                        # add the temporal edge
+                        el = _add_contact(el, i, j, t)
+
+                        # update the probability of moving 
+                        p_not_move[i] = np.max([p_not_move[i], a[j]])
+                        p_not_move[j] = np.max([p_not_move[j], a[i]])
+
+        # move the agents
+        move = np.where(np.random.binomial(1, 1-p_not_move) == 1)[0]
+        theta = np.random.uniform(0, 2*np.pi, len(move))
+        xpos[move] += step*np.cos(theta)
+        ypos[move] += step*np.sin(theta)
+
+        # apply PBC
+        xpos[xpos > L] -= L
+        xpos[xpos < 0] += L
+        ypos[ypos > L] -= L
+        ypos[ypos < 0] += L
+
+    # convert to the ijttau format
+    el_ = []
+
+    for e in el.keys():
+        i, j = e
+        for tt in el[e]:
+            el_.append([i, j, tt[0], tt[1]-tt[0]])
+
+    # construct the dataframe in the form ijttau
+    dfttau = pd.DataFrame(el_, columns = ['i', 'j', 't', 'τ'])
+
+    return dfttau, el
+
+        
